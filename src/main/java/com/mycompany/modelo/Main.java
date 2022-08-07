0a02f13f92606e6f84b4c651d034e539b2fbfa95
damm2001 <dianmart@espol.edu.ec>
2022-08-07 10:06:11 -0500

    toques finales

--- src/main/java/com/mycompany/modelo/Main.java
+++ src/main/java/com/mycompany/modelo/Main.java
@@ -7,7 +7,7 @@ package com.mycompany.modelo;
 
 import java.util.LinkedList;
 import com.mycompany.TDAS.*;
-import java.util.Scanner;
+
 
 /**
  *
@@ -17,55 +17,9 @@ public class Main {
     public static void main(String Args[]) throws CloneNotSupportedException{
 
         BinaryTree<String> BinaryTreeQuestion = Partida.generarArbolJuego("documents/preguntasPrueba.txt", "documents/respuestasPrueba.txt");
+                
+        Partida.empezarJuego(BinaryTreeQuestion);
         
-        LinkedList<String> breadthTraversal = BinaryTreeQuestion.breadthTraversal();
-        for(String s : breadthTraversal){
-            System.out.println(s);
-        }
-        
-        //IMPLEMENTACION DEL JUEGO
-        System.out.println("//////////////////////// ADIVINA EL ANIMAL  ////////////////////////");
-        System.out.println("\nA continuación se le mostrará una serie de preguntas a la cuales deberá responder con Si o No");
-
-        
-        BinaryTree<String> arbolActual= BinaryTreeQuestion;
-        boolean gameOver= false;
-        while(gameOver==false){
-            
-            System.out.println(arbolActual.getRootContent());
-            
-            Scanner sc = new Scanner(System.in); 
-            System.out.println("Ingrese su respuesta: ");
-            String resp = sc.nextLine().toLowerCase();
-
-            if(resp.equals("si")){
-
-                if(arbolActual.getRight()==null){
-                    System.out.println("No se ha encontrado un animal de tales características");
-                    System.out.println("//////////////////////// GAME OVER ////////////////////////");
-                    gameOver=true;
-                }else{
-                    arbolActual=arbolActual.getRight();
-                }
-            }else{
-
-                if(arbolActual.getLeft()==null){
-                    System.out.println("No se ha encontrado un animal de tales características");
-                    System.out.println("//////////////////////// GAME OVER ////////////////////////");
-                    gameOver=true;
-                }else{
-                    arbolActual=arbolActual.getLeft();
-                }
-            }
-
-            
-            if(arbolActual.isLeaf()){
-                System.out.println("¡El animal que estás pensando es un "+arbolActual.getRootContent()+"!");
-                System.out.println("//////////////////////// GAME OVER ////////////////////////");
-                gameOver=true;
-            }
-
-        }
        
     }
     
--- src/main/java/com/mycompany/modelo/Partida.java
+++ src/main/java/com/mycompany/modelo/Partida.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Partida {
@@ -31,10 +32,10 @@ public class Partida {
         
         BinaryTree<String> BinaryTreeQuestion = Utilidades.crearBinaryTreePreguntas(pilaPregs);
         Map<String, Queue<String>> mapaRespuestas = Utilidades.createMapSheets(rutaResp);
-        
+        /*
         for(String animal : mapaRespuestas.keySet()){
             System.out.println(animal + " : " + mapaRespuestas.get(animal));
-        }
+        }*/
         
         for (String clave : mapaRespuestas.keySet()){
             Queue<String> answerTemp = mapaRespuestas.get(clave);
@@ -43,6 +44,53 @@ public class Partida {
         }
         
         return BinaryTreeQuestion;
+    }
+    
+    public static void empezarJuego(BinaryTree<String> BinaryTreeQuestion){
+        //IMPLEMENTACION DEL JUEGO
+        System.out.println("//////////////////////// ADIVINA EL ANIMAL  ////////////////////////");
+        System.out.println("\nA continuación se le mostrará una serie de preguntas a la cuales deberá responder con Si o No\n");
+
+        
+        BinaryTree<String> arbolActual= BinaryTreeQuestion;
+        boolean gameOver= false;
+        while(gameOver==false){
+            
+            System.out.println(arbolActual.getRootContent());
+            
+            Scanner sc = new Scanner(System.in); 
+            System.out.println("Ingrese su respuesta: ");
+            String resp = sc.nextLine().toLowerCase();
+
+            if(resp.equals("si")){
+
+                if(arbolActual.getRight()==null){
+                    System.out.println("\nNo se ha encontrado un animal de tales características\n");
+                    System.out.println("//////////////////////// GAME OVER ////////////////////////");
+                    gameOver=true;
+                }else{
+                    arbolActual=arbolActual.getRight();
+                }
+            }else{
+
+                if(arbolActual.getLeft()==null){
+                    System.out.println("\nNo se ha encontrado un animal de tales características\n");
+                    System.out.println("//////////////////////// GAME OVER ////////////////////////");
+                    gameOver=true;
+                }else{
+                    arbolActual=arbolActual.getLeft();
+                }
+            }
+
+            
+            if(arbolActual.isLeaf()){
+                System.out.println("\n¡El animal que estás pensando es un "+arbolActual.getRootContent()+"!\n");
+                System.out.println("//////////////////////// GAME OVER ////////////////////////");
+                gameOver=true;
+            }
+
+        }
+        
     }
     
     
