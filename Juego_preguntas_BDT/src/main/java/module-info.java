7d439e239e9b4ea94b6160843dac3f4257c6cd6b
Juan Guadalupe <91928959+rexman10@users.noreply.github.com>
2022-08-18 10:56:22 -0500

    Delete Juego_preguntas_BDT directory

--- Juego_preguntas_BDT/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.juego_reguntas_bdt_gui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.juego_reguntas_bdt_gui to javafx.fxml;
-    exports com.mycompany.juego_reguntas_bdt_gui;
-}
