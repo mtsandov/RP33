ccbda72dfe63639b778860ee3020fe8d4c2cfce4
Meiyin Chang <mmchang@espol.edu.ec>
2022-08-24 01:29:19 -0500

    cambios

--- src/main/java/com/mycompany/juego_reguntas_bdt_gui/InicioJuegoController.java
+++ src/main/java/com/mycompany/juego_reguntas_bdt_gui/InicioJuegoController.java
@@ -2,23 +2,52 @@
 package com.mycompany.juego_reguntas_bdt_gui;
 
 import com.mycompany.juego_reguntas_bdt_gui.App;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
 
 
 public class InicioJuegoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    
+    @FXML
+    private Button btnJugar;
+    @FXML
+    private Button btnRespuestaFile;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    @FXML
+    void elegirPreguntas(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Buscar archivo");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("TXT", "*.txt"));
+
+        File preguntasFile = fileChooser.showOpenDialog(null);
+        //System.out.println("file"+preguntasFile.getPath());
+        btnRespuestaFile.setDisable(false);
+    }
+    @FXML
+    void elegirRespuestas(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Buscar archivo");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("TXT", "*.txt"));
+
+        File respuestasFile = fileChooser.showOpenDialog(null);
+        //System.out.println("file"+respuestasFile.getPath());
+        btnJugar.setDisable(false);
+    }   
+
     
     @FXML
     private void empezarPartida(ActionEvent event) throws IOException {
